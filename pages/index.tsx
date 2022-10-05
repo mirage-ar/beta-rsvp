5abb58f69db3800e9496261a7056b3fbbeb872e7
fiigmnt <fiigmnt@protonmail.com>
2022-10-05 16:44:34 -0400

    adding playsinline

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const Rsvp: NextPage = () => {
   return (
     <>
-      <video autoPlay muted loop className={styles.video}>
+      <video playsinline autoPlay muted loop className={styles.video}>
         <source src="/videos/background.mp4" />
       </video>
       <div className={styles.container}>
