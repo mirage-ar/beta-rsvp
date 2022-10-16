cceb0cdd0c15a7e3b66a6b9abc506978331d8ab2
fiigmnt <fiigmnt@protonmail.com>
2022-10-16 12:05:54 -0400

    added testflight link

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/beta',
+        destination: 'https://testflight.apple.com/join/SodgdzA0',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
